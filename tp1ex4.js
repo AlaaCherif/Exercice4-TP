5b28ff43882827772d95321ec8e2b74d66a3c565
Ali-Doggaz <charfedine611@gmail.com>
2021-03-13 10:49:44 +0100

    Improved UI

--- tp1ex4.js
+++ tp1ex4.js
@@ -1,5 +1,5 @@
 function getRndColor() {
-    return 'hsl(' + (360 * Math.random()) + ',50%,50%)'; // H,S,L
+    return 'hsl(' + (360 * Math.random()) +',' + Math.floor(100 * Math.random())+'%,'+ Math.floor(100 * Math.random()) +'%)'; // H,S,L
 }
 var x = document.querySelector(".nn");
 x.addEventListener('click', (e)=> e.target.style.color=getRndColor());
